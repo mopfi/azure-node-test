47f47b5e9fd52fdca2debb5cddf8486d583fc04a
mopfi <mattipe@gmail.com>
2017-12-31 21:58:15 +0200

    add express & fis db query

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Node.js tests on Azure
 
 Check: [Azure App Service Web App](https://docs.microsoft.com/azure/app-service-web)
+
+## Running locally
+
+```sh
+npm install
+cp config-sample.js config.js # and edit config.js
+node index.js
+```
+
+## Deploy to Azure
+
+e.g:
+
+```sh
+git remote add azure https://<USERNAME>@<APPNAME>.scm.azurewebsites.net/<APPNAME>.git
+git commit -am "add config.js" # conf file is needed for db connection
+git push azure master
+```
--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-const http = require('http');
+const Connection = require('tedious').Connection;
+const Request = require('tedious').Request;
+const config = require('./config');
+const dbConnection = new Connection(config);
+const express = require('express')
+const app = express()
 
-const server = http.createServer((request, response) => {
+dbConnection.on('connect', err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('db ready..');
+  }
+});
 
-    response.writeHead(200, {'Content-Type': 'text/html'});
-    response.end('<p>Morjes!</p>');
-
+app.get('/', (req, res) => {
+    queryDatabase(res);
 });
 
 const port = process.env.PORT || 1337;
-server.listen(port);
+app.listen(port, () => console.log('app listening on port 00! at http://localhost:%d', port))
 
-console.log("Server running at http://localhost:%d", port);
+function queryDatabase(res) {
+    console.log('Reading rows from the Table...');
+    let htmlData = '<html><body><h2>Catalog top 10 products</h2><ol>';
+
+    // Read all rows from table
+    request = new Request(
+        "SELECT TOP 10 pc.Name as CategoryName, p.name as ProductName FROM [SalesLT].[ProductCategory] pc JOIN [SalesLT].[Product] p ON pc.productcategoryid = p.productcategoryid",
+        function (err, rowCount, rows) {
+            console.log(rowCount + ' row(s) returned');
+            res.set('Content-Type', 'text/html');
+            res.send(htmlData + '</ol></body></html>')
+        }
+    );
+
+    request.on('row', function (columns) {
+        htmlData += '<li>';
+        columns.forEach(function (column) {
+            htmlData += column.value + ' ';
+            //console.log("%s\t%s", column.metadata.colName, column.value);
+        });
+        htmlData += '</li>';
+    });
+    dbConnection.execSql(request);
+}
